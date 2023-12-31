0b7d7f2970ed80a4dc76f939a4f941afb9ebabc9
kiki03st <rkdtjddbs0347@gmail.com>
2024-01-01 05:10:40 +0900

    test

--- test.swift
+++ test.swift
@@ -1,1 +1,13 @@
 print("Hello Swift!")
+//var : 변수, 값 변경 가능, 큰따옴표("")로 둘러싸인 경우 문자열(String)으로 간주
+var name = "Hwang Hyun"
+//let : 상수, 값 변경 불가능
+let birthyear = 1994
+name = "Mark"
+var languages = ["Swift", "Objective-C", "Python"]
+var capitals = [
+	"한국" : "서울", 
+	"일본" : "도쿄", 
+	"중국" : "베이징",
+]
+print(name)
